0ff2739c147a42b4453030fb2b1acd04ca0a6d9e
William Hesch <wphesch@missionfocus.com>
2016-06-17 17:14:30 -0400

    Bumping version to 1.3.0

--- src/main/scala/com/wordnik/swagger/codegen/BasicScalaGenerator.scala
+++ src/main/scala/com/wordnik/swagger/codegen/BasicScalaGenerator.scala
@@ -211,7 +211,7 @@ class BasicScalaGenerator extends BasicGenerator {
    **/
   additionalParams ++= Map(
     "artifactId" -> "semapp-scala-client",
-    "apiVersion" -> ("1.3.0-SNAPSHOT"),
+    "apiVersion" -> ("1.3.0"),
     "groupId" -> "org.imintel.client")
 
   // supporting classes
